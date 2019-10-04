12383fa394a582480fd33b27a4786f3251223ccd
Matheus Figueiredo <matheus_figueiredo18@hotmail.com>
2019-10-03 22:27:55 -0300

    Adicao do Icone e dos campos de adicao de texto (Peso e Altura). Inicio do corpo.

--- calculadora_imc/lib/main.dart
+++ calculadora_imc/lib/main.dart
@@ -28,8 +28,25 @@ class _HomeState extends State<Home> {
       backgroundColor: Colors.white,      // Cor de fundo do corpo (tela abaixo da barra)
 
       body: Column(         // Corpo do app
+        crossAxisAlignment: CrossAxisAlignment.stretch,       // Alarga tudo da coluna, preenche todo o espaco
         children: <Widget>[
-          Icon(Icons.person_outline, size: 120.0, color: Colors.green,)
+          Icon(Icons.person_outline, size: 120.0, color: Colors.green),
+          TextField(keyboardType: TextInputType.number,     // Para o usuario digitar um numero
+            decoration: InputDecoration(                    // Para mostrar uma msg no campo de digitacao 
+              labelText: "Peso (kg)",
+              labelStyle: TextStyle(color: Colors.green)
+            ),
+            textAlign: TextAlign.center,
+            style: TextStyle(color: Colors.green, fontSize: 25.0),
+          ),
+          TextField(keyboardType: TextInputType.number,
+            decoration: InputDecoration(
+              labelText: "Altura (cm)",
+              labelStyle: TextStyle(color: Colors.green)
+            ),
+            textAlign: TextAlign.center,
+            style: TextStyle(color: Colors.green, fontSize: 25.0),
+          )
         ],
       ),
     );
