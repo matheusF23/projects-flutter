50feee4361e0c6afce5327f914a71f29e8d719b1
Matheus Figueiredo <matheus_figueiredo18@hotmail.com>
2019-10-04 22:35:04 -0300

    Adição do botão 'Calcular'

--- calculadora_imc/lib/main.dart
+++ calculadora_imc/lib/main.dart
@@ -1,12 +1,13 @@
 import 'package:flutter/material.dart';
 
-void main(){
+void main() {
   runApp(MaterialApp(
     home: Home(),
   ));
 }
 
-class Home extends StatefulWidget {     // Tela inicial
+class Home extends StatefulWidget {
+  // Tela inicial
   @override
   _HomeState createState() => _HomeState();
 }
@@ -15,35 +16,51 @@ class _HomeState extends State<Home> {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      appBar: AppBar(           // Barra superior
+      appBar: AppBar(
         title: Text("Calculadora de IMC"),
-        centerTitle: true,      // Centraliza o texto
-        backgroundColor: Colors.green,      // Modifica a cor da barra
-        actions: <Widget>[      // Para colocar os botoes na barra
-          IconButton(icon: Icon(Icons.refresh),
-          onPressed: () {},)
+        centerTitle: true,
+        backgroundColor: Colors.green,
+        actions: <Widget>[
+          IconButton(
+            icon: Icon(Icons.refresh),
+            onPressed: () {},
+          )
         ],
       ),
-
-      backgroundColor: Colors.white,      // Cor de fundo do corpo (tela abaixo da barra)
-
-      body: Column(         // Corpo do app
-        crossAxisAlignment: CrossAxisAlignment.stretch,       // Alarga tudo da coluna, preenche todo o espaco
+      backgroundColor: Colors.white,
+      body: Column(
+        crossAxisAlignment: CrossAxisAlignment.stretch,
         children: <Widget>[
           Icon(Icons.person_outline, size: 120.0, color: Colors.green),
-          TextField(keyboardType: TextInputType.number,     // Para o usuario digitar um numero
-            decoration: InputDecoration(                    // Para mostrar uma msg no campo de digitacao 
-              labelText: "Peso (kg)",
-              labelStyle: TextStyle(color: Colors.green)
-            ),
+          TextField(
+            keyboardType: TextInputType.number,
+            decoration: InputDecoration(
+                labelText: "Peso (kg)",
+                labelStyle: TextStyle(color: Colors.green)),
             textAlign: TextAlign.center,
             style: TextStyle(color: Colors.green, fontSize: 25.0),
           ),
-          TextField(keyboardType: TextInputType.number,
+          TextField(
+            keyboardType: TextInputType.number,
             decoration: InputDecoration(
-              labelText: "Altura (cm)",
-              labelStyle: TextStyle(color: Colors.green)
+                labelText: "Altura (cm)",
+                labelStyle: TextStyle(color: Colors.green)),
+            textAlign: TextAlign.center,
+            style: TextStyle(color: Colors.green, fontSize: 25.0),
+          ),
+          Container(
+            height: 50.0,
+            child: RaisedButton(
+              onPressed: () {},
+              child: Text(
+                "Calcular",
+                style: TextStyle(color: Colors.white, fontSize: 25.0),
+              ),
+              color: Colors.green,
             ),
+          ),
+          Text(
+            "Info",
             textAlign: TextAlign.center,
             style: TextStyle(color: Colors.green, fontSize: 25.0),
           )
