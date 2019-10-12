4acc1b673b42df75714225c677b21bde9eb1a14f
Matheus Figueiredo <matheus_figueiredo18@hotmail.com>
2019-10-11 22:55:19 -0300

    Busca dos dados da api.

--- conversor/lib/main.dart
+++ conversor/lib/main.dart
@@ -2,13 +2,14 @@ import 'package:flutter/material.dart';
 
 import 'package:http/http.dart' as http; // Permite fazer as requisicoes
 import 'package:async/async.dart'; // Permite fazer requisicoes sem ter que espera-las. Requisicao assincrona.
+import 'dart:convert';
 
 const request = "https://api.hgbrasil.com/finance/quotations?key=cae60900";
 
 void main() async{
 
   http.Response response = await http.get(request);
-  print(response.body);
+  print(json.decode(response.body)["results"]["currencies"]["USD"]);
 
   runApp(MaterialApp(
     home: Container(),
