6aeb3b4bbb30baade48195b08504cb62d332a576
Matheus Figueiredo <matheus_figueiredo18@hotmail.com>
2019-10-12 22:33:48 -0300

    Otimizacao do codigo e adicao de controladores.

--- conversor/lib/main.dart
+++ conversor/lib/main.dart
@@ -24,8 +24,24 @@ class Home extends StatefulWidget {
 }
 
 class _HomeState extends State<Home> {
+  final realController = TextEditingController();
+  final dolarController = TextEditingController();
+  final euroController = TextEditingController();
+
   double dolar;
   double euro;
+
+  void _realChanged(String text) {
+    print(text);
+  }
+
+  void _dolarChanged(String text) {
+    print(text);
+  }
+
+  void _euroChanged(String text) {
+    print(text);
+  }
 
   @override
   Widget build(BuildContext context) {
@@ -65,32 +81,14 @@ class _HomeState extends State<Home> {
                       children: <Widget>[
                         Icon(Icons.monetization_on,
                             size: 150.0, color: Colors.amber),
-                        TextField(
-                          decoration: InputDecoration(
-                              labelText: "Reais",
-                              labelStyle: TextStyle(color: Colors.amber),
-                              border: OutlineInputBorder(),
-                              prefixText: "R\$"),
-                          style: TextStyle(color: Colors.amber, fontSize: 25.0),
-                        ),
+                        buildTextFild(
+                            "Reais", "R\$", realController, _realChanged),
                         Divider(),
-                        TextField(
-                          decoration: InputDecoration(
-                              labelText: "Dolares",
-                              labelStyle: TextStyle(color: Colors.amber),
-                              border: OutlineInputBorder(),
-                              prefixText: "US\$"),
-                          style: TextStyle(color: Colors.amber, fontSize: 25.0),
-                        ),
+                        buildTextFild(
+                            "Dolares", "US\$", dolarController, _dolarChanged),
                         Divider(),
-                        TextField(
-                          decoration: InputDecoration(
-                              labelText: "Euros",
-                              labelStyle: TextStyle(color: Colors.amber),
-                              border: OutlineInputBorder(),
-                              prefixText: "£"),
-                          style: TextStyle(color: Colors.amber, fontSize: 25.0),
-                        )
+                        buildTextFild(
+                            "Euros", "£", euroController, _euroChanged)
                       ],
                     ),
                   );
@@ -99,4 +97,19 @@ class _HomeState extends State<Home> {
           }),
     );
   }
+}
+
+Widget buildTextFild(
+    String label, String prefix, TextEditingController c, Function f) {
+  return TextField(
+    controller: c,
+    decoration: InputDecoration(
+        labelText: label,
+        labelStyle: TextStyle(color: Colors.amber),
+        border: OutlineInputBorder(),
+        prefixText: prefix),
+    style: TextStyle(color: Colors.amber, fontSize: 25.0),
+    onChanged: f,
+    keyboardType: TextInputType.number, // Colocar apenas numeros
+  );
 }
