fac2263f87bde2f1a69029943d198012d6477a25
Matheus Figueiredo <matheus_figueiredo18@hotmail.com>
2019-10-14 22:29:01 -0300

    Início do layout.

--- lista_de_tarefas/lib/main.dart
+++ lista_de_tarefas/lib/main.dart
@@ -19,7 +19,35 @@ class _HomeState extends State<Home> {
 
   @override
   Widget build(BuildContext context) {
-    return Container();
+    return Scaffold(
+      appBar: AppBar(
+        title: Text("Lista de Tarefas"),
+        backgroundColor: Colors.blueAccent,
+        centerTitle: true,
+      ),
+      body: Column(
+        children: <Widget>[
+          Container(
+            padding: EdgeInsets.fromLTRB(17.0, 1.0, 7.0, 1.0),
+            child: Row(
+              children: <Widget>[
+                Expanded(child: TextField(
+                  decoration: InputDecoration(
+                      labelText: "Nova Tarefa",
+                      labelStyle: TextStyle(color: Colors.blueAccent)),
+                ),),
+                RaisedButton(
+                  color: Colors.blueAccent,
+                  child: Text("ADD"),
+                  textColor: Colors.white,
+                  onPressed: (){},
+                )
+              ],
+            ),
+          )
+        ],
+      ),
+    );
   }
 
   Future<File> _getFile() async {
@@ -35,7 +63,7 @@ class _HomeState extends State<Home> {
   }
 
   Future<String> _readData() async {
-    try{
+    try {
       final file = await _getFile();
       return file.readAsString();
     } catch (e) {
