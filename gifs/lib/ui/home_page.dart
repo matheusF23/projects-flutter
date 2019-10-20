8bad8e4962b39394e5dafcfe3315143f1d5f643e
Matheus Figueiredo <matheus_figueiredo18@hotmail.com>
2019-10-19 22:52:01 -0300

    Imports - HomePage and http packages.

--- gifs/lib/ui/home_page.dart
+++ gifs/lib/ui/home_page.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:http/http.dart' as http;
 
 class HomePage extends StatefulWidget {
   @override
@@ -6,6 +7,11 @@ class HomePage extends StatefulWidget {
 }
 
 class _HomePageState extends State<HomePage> {
+  
+  _getGifs() async{
+    http.Response response;
+  }
+  
   @override
   Widget build(BuildContext context) {
     return Container(
