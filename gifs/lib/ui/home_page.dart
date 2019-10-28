285275afca1f59b732e62ea6bba7db2df853d451
Matheus Figueiredo <matheus_figueiredo18@hotmail.com>
2019-10-28 20:13:51 -0300

    Criação do método _createGifTable para mostrar os gifs na tela

--- gifs/lib/ui/home_page.dart
+++ gifs/lib/ui/home_page.dart
@@ -75,13 +75,33 @@ class _HomePageState extends State<HomePage> {
                       ),
                     );
                   default:
-                    return Container();
+                    if (snapshot.hasError)
+                      return Container();
+                    else
+                      return _createGifTable(context, snapshot);
                 }
               },
             ),
           )
         ],
       ),
+    );
+  }
+
+  Widget _createGifTable(BuildContext context, AsyncSnapshot snapshot) {
+    return GridView.builder(
+      padding: EdgeInsets.all(10.0),
+      gridDelegate: SliverGridDelegateWithFixedCrossAxisCount(
+          crossAxisCount: 2, crossAxisSpacing: 10.0, mainAxisSpacing: 10.0),
+      itemCount: snapshot.data["data"].length,
+      itemBuilder: (context, index) {
+        return GestureDetector(
+          child: Image.network(
+              snapshot.data["data"][index]["images"]["fixed_height"]["url"],
+              height: 300.0,
+              fit: BoxFit.cover),
+        );
+      },
     );
   }
 }
