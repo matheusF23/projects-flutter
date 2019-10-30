327e073889ef97890017537ee310de8300286eac
Matheus Figueiredo <matheus_figueiredo18@hotmail.com>
2019-10-29 22:30:40 -0300

    Adição da opção de compartilhamento

--- gifs/lib/ui/home_page.dart
+++ gifs/lib/ui/home_page.dart
@@ -3,6 +3,7 @@ import 'dart:convert';
 import 'package:flutter/material.dart';
 import 'package:gifs/ui/gif_page.dart';
 import 'package:http/http.dart' as http;
+import 'package:share/share.dart';
 
 class HomePage extends StatefulWidget {
   @override
@@ -122,6 +123,10 @@ class _HomePageState extends State<HomePage> {
                   MaterialPageRoute(
                       builder: (context) =>
                           GifPage(snapshot.data["data"][index])));
+            },
+            onLongPress: () {
+              Share.share(snapshot.data["data"][index]["images"]["fixed_height"]
+                  ["url"]);
             },
           );
         else
