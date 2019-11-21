095e33406e9a054f74c12a547f5843409e497783
Matheus Figueiredo <matheus_figueiredo18@hotmail.com>
2019-11-20 22:39:50 -0300

    Adição da funcionalidade de abrir a câmera para add uma foto.

--- agenda_contato/lib/ui/contact_page.dart
+++ agenda_contato/lib/ui/contact_page.dart
@@ -2,6 +2,7 @@ import 'dart:io';
 
 import 'package:agenda_contato/helpers/contact_helper.dart';
 import 'package:flutter/material.dart';
+import 'package:image_picker/image_picker.dart';
 
 class ContactPage extends StatefulWidget {
   final Contact contact;
@@ -74,6 +75,15 @@ class _ContactPageState extends State<ContactPage> {
                             : AssetImage("images/person.jpg")),
                   ),
                 ),
+                onTap: () {
+                  ImagePicker.pickImage(source: ImageSource.camera)
+                      .then((file) {
+                    if (file == null) return;
+                    setState(() {
+                      _editedContact.img = file.path;
+                    });
+                  });
+                },
               ),
               TextField(
                 controller: _nameController,
