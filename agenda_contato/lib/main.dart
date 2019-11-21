3e4989581228013044402505b0b0522765a4216d
Matheus Figueiredo <matheus_figueiredo18@hotmail.com>
2019-11-20 23:02:20 -0300

    Adição da ordenação por nome dos contatos.

--- agenda_contato/lib/main.dart
+++ agenda_contato/lib/main.dart
@@ -2,7 +2,7 @@ import 'package:agenda_contato/ui/contact_page.dart';
 import 'package:agenda_contato/ui/home_page.dart';
 import 'package:flutter/material.dart';
 
-void main(){
+void main() {
   runApp(MaterialApp(
     home: HomePage(),
     debugShowCheckedModeBanner: false,
