5a7c3b4ad00d097b7a42ffd6a8bd2236701da7c3
Matheus Figueiredo <matheus_figueiredo18@hotmail.com>
2019-12-17 18:54:39 -0300

    Definindo o Tema.

--- chat_online/lib/main.dart
+++ chat_online/lib/main.dart
@@ -2,20 +2,53 @@ import 'package:flutter/material.dart';
 import 'package:cloud_firestore/cloud_firestore.dart';
 
 Future main() async {
-  Firestore.instance.collection("mensagens").snapshots().listen((snapshot){
-
-        for(DocumentSnapshot doc in snapshot.documents){
-          print(doc.data);
-        }
-        
-      });
-
   runApp(MyAppp());
 }
+
+final ThemeData kIOSTheme = ThemeData(
+    primarySwatch: Colors.orange,
+    primaryColor: Colors.grey[100],
+    primaryColorBrightness: Brightness.light);
+
+final ThemeData kDefaultTheme = ThemeData(
+    primarySwatch: Colors.purple, accentColor: Colors.orangeAccent[400]);
 
 class MyAppp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
-    return Container();
+    return MaterialApp(
+      title: "Chat App",
+      debugShowCheckedModeBanner: false,
+      theme: Theme.of(context).platform == TargetPlatform.iOS
+          ? kIOSTheme
+          : kDefaultTheme,
+      home: ChatScreen(),
+    );
+  }
+}
+
+class ChatScreen extends StatefulWidget {
+  @override
+  _ChatScreenState createState() => _ChatScreenState();
+}
+
+class _ChatScreenState extends State<ChatScreen> {
+  @override
+  Widget build(BuildContext context) {
+    return SafeArea(
+      bottom: false,
+      top: false,
+      child: Scaffold(
+        appBar: AppBar(
+          title: Text("Chat App"),
+          centerTitle: true,
+          elevation:
+              Theme.of(context).platform == TargetPlatform.iOS ? 0.0 : 4.0,
+        ),
+        body: Column(
+          
+        ),
+      ),
+    );
   }
 }
